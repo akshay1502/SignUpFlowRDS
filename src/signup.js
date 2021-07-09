8be22c55d3738fca64f17a9e8292d660dc33d383
akshay shinde <akshayshinde7289@gmail.com>
2021-07-09 18:43:26 +0530

    validating email and showing msg to user

--- src/signup.js
+++ src/signup.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 const Signup = () => {
+    const [ msg, setMsg] = useState('');
+    const emailRegex = /\S+@\S+\.\S+/;
+    const validateEmail = () => {
+        const email = document.getElementById('email');
+        if (emailRegex.test(email.value)) {
+            email.style.border = `2px solid green`;
+            setMsg('');
+        } else {
+            email.style.border = ``;
+            setMsg('Please enter a valid email!');
+        }
+    };
     return ( 
         <div>
             <form>
                 <div className="inputfields">
-                    <label>Email</label>
-                    <input type="email" placeholder="type your mail here..." required/>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" placeholder="type your mail here..." id="email" name="email" onBlur={validateEmail} required/>
+                    <p style={{color:"red"}}>{msg}</p>
                 </div>
                 <div className="inputfields">
                     <label>Password</label>
